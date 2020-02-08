6d7efb47bd7b5779e81236ecef11fffdef6938bd
yannicklerestif <yannick1976.git@gmail.com>
2020-02-08 23:02:11 +0100

    adding some first sprites

---
+++ Assets/Scenes/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Debug.Log($"h: {h} - v: {v}");
+
+        gameObject.transform.position = new Vector2(
+            transform.position.x + h * speed,
+            transform.position.y + v * speed
+        );
+    }
+}
